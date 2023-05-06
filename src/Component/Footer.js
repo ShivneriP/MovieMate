ecd2cb8dfba95ce85f2c4617bd929fc536e00961
shivneri <toraskarshivneri@gmail.com>
2023-05-06 13:57:09 +0530

    Updated useEffect Dependency

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -11,16 +11,16 @@ export const Footer = () => {
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-            <a href="https://www.facebook.com/shivneri.zambare" target='_blank' className="mr-4 hover:underline md:mr-6 ">Instagram</a>
+            <a href="https://www.facebook.com/shivneri.zambare" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6 ">Instagram</a>
         </li>
         <li>
-            <a href="https://www.facebook.com/shivneri.zambare" target='_blank' className="mr-4 hover:underline md:mr-6">LinkedIn</a>
+            <a href="https://www.facebook.com/shivneri.zambare" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
         </li>
         <li>
-            <a href="https://www.facebook.com/shivneri.zambare" target='_blank' className="mr-4 hover:underline md:mr-6">YouTube</a>
+            <a href="https://www.facebook.com/shivneri.zambare" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">YouTube</a>
         </li>
         <li>
-            <a href="https://www.facebook.com/shivneri.zambare"  target='_blank' className="hover:underline">GitHub</a>
+            <a href="https://www.facebook.com/shivneri.zambare"  target="_blank" rel="noreferrer" className="hover:underline">GitHub</a>
         </li>
     </ul>
     </div>
